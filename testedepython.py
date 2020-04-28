8b8b701f9cbd7f11d6ac3ce103c02d62984da20c
SrCaveiraOFC <64497000+SrCaveiraOFC@users.noreply.github.com>
2020-04-28 17:18:56 -0300

    Update testedepython.py

--- testedepython.py
+++ testedepython.py
@@ -1,5 +1,5 @@
 nome = input('Digite seu Nome: ')
 print('Seja Bem-Vindo,', nome!)
-pergunta = input('Você deseja virar um Hacker?')
-print = ('Entendi, então a resposta é:', pergunta')
-print('Ok, entendi. Bem, você irá ficar querendo ser, hahaha!')
+pergunta = input('Você deseja virar um Hacker?: ')
+print('Entendi, então a resposta é:', pergunta')
+print('Bem, você irá ficar querendo ser, hahaha!')
